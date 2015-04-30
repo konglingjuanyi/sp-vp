bcd29b0300aefc21b080203abc5c926c9e1ea5db
hwyz_leo <yerongjie_hwyz_leo@aliyun.com>
2015-04-30 14:40:32 +0800

    add EventDao Interface, Implement, Repository, Mapper and Test
    Change-Id: I11a2f2d66499c70919dbd77d60bdd3926f36cfb1

---
+++ sp-vp-service-dao/src/main/java/com/zxq/iov/cloud/sp/vp/dao/IEventDaoService.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2007-2011 SAIC. All Rights Reserved.
+ * This software is published under the terms of the SAIC IS Dept.
+ * 
+ * @Project: 
+ * @Title: IVehicleService.java
+ * @Package com.saicmotor.telematics.tsgp.tcmp.vehicle.service
+ * @Description: 
+ *
+ * @CreateDate : 2012-2-2
+ * @CreateBy   : Administrator
+ */
+package com.zxq.iov.cloud.sp.vp.dao;
+
+import com.zxq.iov.cloud.core.service.BaseService;
+import com.zxq.iov.cloud.sp.vp.entity.Vehicle;
+import com.zxq.iov.cloud.sp.vp.entity.event.Event;
+
+/**
+ * 安防 事件持久化服务接口
+ *
+ * @author 叶荣杰
+ * create date 2015-4-29 14:57:07
+ * @version 0.1, 2015-4-29
+ */
+public interface IEventDaoService extends BaseService<Event, Long> {
+
+	/**
+	 * 创建事件
+	 * @param event	事件对象
+	 * @return		事件对象
+	 */
+	Event createEvent(Event event);
+
+	/**
+	 * 更新事件
+	 * @param event	事件对象
+	 * @return		事件对象
+	 */
+	Event updateEvent(Event event);
+
+	/**
+	 * 根据Event ID得到事件对象
+	 * @param eventId 	事件ID
+	 * @return			事件对象
+	 */
+	Event findEventById(Long eventId);
+	
+}
---
+++ sp-vp-service-dao/src/test/java/com/zxq/iov/cloud/sp/vp/service/EventDaoServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.zxq.iov.cloud.sp.vp.service;
+
+import com.zxq.iov.cloud.core.test.BaseServiceTestCase;
+import com.zxq.iov.cloud.sp.vp.dao.IEventDaoService;
+import com.zxq.iov.cloud.sp.vp.dao.IVehicleDaoService;
+import com.zxq.iov.cloud.sp.vp.entity.Vehicle;
+import com.zxq.iov.cloud.sp.vp.entity.event.Event;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tokxp
+ * Date: 14-11-27
+ * Time: 上午8:56
+ */
+@Transactional
+public class EventDaoServiceImplTest extends BaseServiceTestCase {
+
+    @Autowired
+    private IEventDaoService eventDaoService;
+
+    @Test
+    @Rollback(true)
+    public void testCreateEvent(){
+        Event event = new Event();
+        event.setVin("001");
+        event.setCode("1");
+        event = eventDaoService.createEvent(event);
+        Assert.assertNotNull(event);
+    }
+
+    @Test
+    @Rollback(true)
+    public void testUpdateEvent(){
+        Long eventId = 11L;
+        Event event = eventDaoService.findEventById(eventId);
+        event.setVin("002");
+        event = eventDaoService.updateEvent(event);
+        Assert.assertNotNull(event);
+    }
+
+}
